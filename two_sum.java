2dbee09ab038695c1a35c50b7d465805df277ad3
Priyanshu Patel <priyanshupatel2804@gmail.com>
2025-10-03 03:18:31 +0530

    message

---
+++ two_sum.java
@@ -0,0 +1,24 @@
+public class two_sum {
+    public static boolean twosum(int arr[] , int target){
+        for (int i = 0; i<arr.length-1; i++) {
+            for(int j = i+1; j< arr.length; j++){
+                if(arr[i]+arr[j]==target){
+                return true;
+                }
+                
+                }
+                
+            
+                
+            }
+            
+        return false;
+        }       
+    
+    public static void main(String[] args) {
+        int arr[] = {2,4,67,9,4,2,5};
+        int target = 4;
+        boolean s = twosum(arr, target);
+        System.out.println(s);
+    }
+}
